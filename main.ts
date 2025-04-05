0746fb0a2b22e3f53d900240e2fd43bd26908498
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-04-05 21:00:52 +0900

    wip

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 import { TarWriter } from "npm:@gera2ld/tarjs";
 import { walk } from "jsr:@std/fs"
 
+const [target, outName] = Deno.args;
+
 const writer = new TarWriter();
 
-await Promise.all((await Array.fromAsync(walk(".", { includeDirs: false }))).map(({ path }) => new Response(new Response(Deno.readFileSync(path)).body?.pipeThrough(new CompressionStream("gzip"))).arrayBuffer().then(ab => writer.addFile(path, ab))))
+await Promise.all((await Array.fromAsync(walk(`./${target || ""}`, { includeDirs: false }))).map(({ path }) => new Response(new Response(Deno.readFileSync(path)).body?.pipeThrough(new CompressionStream("gzip"))).arrayBuffer().then(ab => writer.addFile(path, ab))))
 
-Deno.writeFileSync("out.gzt", new Uint8Array(await (await writer.write()).arrayBuffer()))
+Deno.writeFileSync(`${outName || "out"}.gzt`, new Uint8Array(await (await writer.write()).arrayBuffer()))
